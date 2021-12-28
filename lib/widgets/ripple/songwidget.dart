45e7a8ea6fc3f4f99f01d61b632fcea30f82ee9e
Armagan <armagan.celik@hotmail.com>
2021-12-28 15:49:57 +0300

    correction process
    correction process

--- lib/widgets/ripple/songwidget.dart
+++ lib/widgets/ripple/songwidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_acrcloud/flutter_acrcloud.dart';
+import 'package:listenbox/widgets/SlideFadeTransition.dart';
 
 class SongWidget extends StatefulWidget {
   final ACRCloudResponseMusicItem music;
@@ -14,18 +15,29 @@ class _SongWidgetState extends State<SongWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          widget.music.title,
-          style: const TextStyle(
-              fontSize: 25.0, color: Colors.black, fontWeight: FontWeight.bold),
+        SlideFadeTransition(
+          delayStart: const Duration(milliseconds: 450),
+          child: Text(
+            widget.music.title,
+            style: const TextStyle(
+                fontSize: 25.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold),
+          ),
         ),
-        Text(
-          widget.music.album.name,
-          style: const TextStyle(fontSize: 20.0, color: Colors.black),
+        SlideFadeTransition(
+          delayStart: const Duration(milliseconds: 900),
+          child: Text(
+            widget.music.album.name,
+            style: const TextStyle(fontSize: 20.0, color: Colors.black),
+          ),
         ),
-        Text(
-          widget.music.artists.first.name,
-          style: const TextStyle(fontSize: 20.0, color: Colors.black),
+        SlideFadeTransition(
+          delayStart: const Duration(milliseconds: 1350),
+          child: Text(
+            widget.music.artists.first.name,
+            style: const TextStyle(fontSize: 20.0, color: Colors.black),
+          ),
         ),
       ],
     );
