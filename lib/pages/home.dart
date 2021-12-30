e6b913d7f844cef66174974d35fcbc03a64e6ca2
Armagan <armagan4celik@gmail.com>
2021-12-30 16:23:15 +0300

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -164,7 +164,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                                   music = null;
                                   session?.volumeStream.listen((event) {
                                     setState(() {
-                                      _micVolume = (100 * event);
+                                      _micVolume = (150 * event);
                                     });
                                   });
                                 });
